86f9b5b28250d6a079b668c69c796989d91f1f58
ZanJS <suihuasheng@gmail.com>
2020-01-21 16:05:00 +0800

    add app config :relieved:

---
+++ packages/config/client.d.ts
@@ -0,0 +1,13 @@
+declare const config: {
+	APP: {
+		APIPrefix: string;
+	};
+	cookie: string;
+	token: string;
+	Token: string;
+	domain: string;
+	wxappid: string;
+	facebookappid: string;
+};
+
+export default config;
---
+++ packages/config/client.js
@@ -0,0 +1,19 @@
+/**
+ * APP client Config
+ */
+
+const data = {
+	APP: {
+		APIPrefix: 'napi'
+	},
+	cookie: '.aspnetform',
+	token: 'token',
+	Token: 'Token',
+	domain: 'panli.com',
+	wxappid: 'wx3b24496f74bf93d0',
+	facebookappid: '563546080352185'
+};
+
+exports = data;
+
+module.exports = data;
